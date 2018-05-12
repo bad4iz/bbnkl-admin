e1cc476a2ebb3a1706a61c4f3b279eb07bc1a7a0
bad4iz <info@bad4iz.ru>
2018-05-12 20:10:03 +0400

    форма входа

---
+++ src/components/Auth.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <img src="../../static/img/bbnkl-logo2.png" alt="Vuetify.js" class="mb-5">
+
+
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+<script>
+</script>
