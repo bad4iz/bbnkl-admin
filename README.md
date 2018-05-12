c3fac5b070aafe296e189a96a8a9eb4af940a58d
bad4iz <info@bad4iz.ru>
2018-05-12 18:35:41 +0400

    add

---
+++ README.md
@@ -0,0 +1,49 @@
+# bbnkl-admin
+
+> A Vuetify project running in Cordova
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run e2e tests
+npm run e2e
+
+# run all tests
+npm test
+
+# build for production and cordova build.
+npm run cordova-build
+
+# build for production and serve the app through the browser - no hot reload.
+npm run browser
+
+# add respective platforms
+cordova platform add android
+cordova platform add ios
+
+# build for production and serve the app on an iOS device
+npm run ios
+
+# build for production and serve the app on an android device (won't serve on a virtual device)
+npm run android
+
+# build for production and serve the app on an android device (will serve on a virtual device or physical device - prefers virtual)
+npm run android-vm
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,135 @@
+<template>
+  <v-app light>
+    <v-navigation-drawer
+      fixed
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+      app
+    >
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+          exact
+        >
+          <v-list-tile-action>
+            <v-icon light v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar fixed app :clipped-left="clipped">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer" light></v-toolbar-side-icon>
+      <v-btn
+        icon
+        light
+        @click.stop="miniVariant = !miniVariant"
+      >
+        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        light
+        @click.stop="clipped = !clipped"
+      >
+        <v-icon>web</v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        icon
+        light
+        @click.stop="rightDrawer = !rightDrawer"
+      >
+        <v-icon>menu</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+    <v-navigation-drawer
+      temporary
+      :right="right"
+      v-model="rightDrawer"
+      fixed
+    >
+      <v-list>
+        <v-list-tile @click="right = !right">
+          <v-list-tile-action>
+            <v-icon light>compare_arrows</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </v-app>
+</template>
+
+<script>
+  import Vue from 'vue';
+
+  export default {
+    data() {
+      return {
+        cordova: Vue.cordova,
+        clipped: false,
+        drawer: true,
+        items: [{
+          icon: 'bubble_chart',
+          title: 'Inspire',
+        }],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'Vuetify.js',
+      };
+    },
+    created() {
+      const self = this;
+      this.cordova.on('deviceready', () => {
+        self.onDeviceReady();
+      });
+    },
+    methods: {
+      onDeviceReady: function () {
+        // Handle the device ready event.
+        this.cordova.on('pause', this.onPause, false);
+        this.cordova.on('resume', this.onResume, false);
+        if (this.cordova.device.platform === 'Android') {
+          document.addEventListener('backbutton', this.onBackKeyDown, false);
+        }
+      },
+      onPause() {
+        // Handle the pause lifecycle event.
+        console.log('pause');
+      },
+      onResume() {
+        // Handle the resume lifecycle event.
+        // SetTimeout required for iOS.
+        setTimeout(() => {
+          console.log('resume');
+        }, 0);
+      },
+      onBackKeyDown() {
+        // Handle the back-button event on Android. By default it will exit the app.
+        navigator.app.exitApp();
+      },
+    },
+  };
+</script>
+
+<style>
+	body {
+    padding-top: constant(safe-area-inset-top);
+    padding-top: env(safe-area-inset-top);
+	}
+  .footer{ /* Apply this to v-bottom-nav if necessary. */
+    margin-bottom: constant(safe-area-inset-bottom);
+    margin-bottom: env(safe-area-inset-bottom);
+  }
+</style>
